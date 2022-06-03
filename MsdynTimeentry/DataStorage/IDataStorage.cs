90310ff9e9c8216933fd4eca9448be6426c1d2e5
NeuAr <neuar@yandex.ru>
2022-06-04 01:41:04 +0200

    Add data storage interface

---
+++ MsdynTimeentry/DataStorage/IDataStorage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MsdynTimeentry.DataStorage
+{
+    public interface IDataStorage : IDisposable
+    {
+        HashSet<string> GetDatesOfTimeentriesFromRange(DateTime beginDate, DateTime endDate, string resultDatesFormat);
+
+        int CreateTimeentries(IEnumerable<DateTime> dates);
+    }
+}

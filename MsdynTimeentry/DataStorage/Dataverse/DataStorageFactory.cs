0aab031c8dfdd256b9832b5e8121ce245983989b
NeuAr <neuar@yandex.ru>
2022-06-04 23:29:36 +0200

    Add implementation of data storage factory for Dataverse

---
+++ MsdynTimeentry/DataStorage/Dataverse/DataStorageFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MsdynTimeentry.DataStorage.Dataverse
+{
+    internal class DataStorageFactory : IDataStorageFactory
+    {
+        private readonly string connectionString;
+
+        public DataStorageFactory(string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+                throw new ArgumentException("Сonnection string is empty", nameof(connectionString));
+
+            this.connectionString = connectionString;
+        }
+
+        public IDataStorage Create()
+        {
+            return new DataStorage(connectionString);
+        }
+    }
+}

8c55c7b3a508146e74a7a99609758c5db174d553
NeuAr <neuar@yandex.ru>
2022-06-05 12:50:31 +0200

    Add HTTP requests converter

---
+++ MsdynTimeentry/Http/RequestsConverter.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Schema;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MsdynTimeentry.Http
+{
+    internal static class RequestsConverter
+    {
+        public static async Task<TTarget> ConvertBodyTo<TTarget>(HttpRequest req, string jsonSchema = null)
+        {
+            if (req is null)
+                throw new ArgumentNullException(nameof(req), "HTTP request is null");
+
+            string requestBodyJson = await new StreamReader(req.Body).ReadToEndAsync();
+            if (string.IsNullOrWhiteSpace(requestBodyJson))
+                throw new ArgumentException("HTTP request body is empty", nameof(req));
+
+            JsonSerializer serializer = new JsonSerializer();
+            JsonTextReader jsonTextReader = new JsonTextReader(new StringReader(requestBodyJson));
+
+            if (!string.IsNullOrEmpty(jsonSchema))
+            {
+                JSchemaValidatingReader jsonSchemaValidatingReader = new JSchemaValidatingReader(jsonTextReader)
+                {
+                    Schema = JSchema.Parse(jsonSchema)
+                };
+
+                return serializer.Deserialize<TTarget>(jsonSchemaValidatingReader);
+            }
+            else
+            {
+                return serializer.Deserialize<TTarget>(jsonTextReader);
+            }
+        }
+    }
+}

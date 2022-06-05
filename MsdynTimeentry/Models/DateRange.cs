358f92d916f5e585264b5e989c24d899a5d8684c
NeuAr <neuar@yandex.ru>
2022-06-05 13:06:07 +0200

    Add model of date range

---
+++ MsdynTimeentry/Models/DateRange.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MsdynTimeentry.Models
+{
+    internal class DateRange
+    {
+        public DateTime StartOn { get; set; }
+
+        public DateTime EndOn { get; set; }
+    }
+}
